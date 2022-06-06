f0c5b6887a6b891db6a864604d410d5f33052bac
Cleverton Teixeira <clevertons.b.t@gmail.com>
2022-06-06 20:01:05 -0300

    Creditos criado

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Category } from './components/category';
+import { Creditos } from './components/creditos';
 import { Emphasis } from './components/emphasis';
 import { Footer } from './components/footer';
 import { Header } from './components/header';
@@ -12,6 +13,7 @@ function App() {
       <Emphasis />
       <Category />
       <Footer />
+      <Creditos />
     </>
   );
 }
---
+++ web/src/components/creditos/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Creditos() {
+  return <p className="py-8 flex justify-center">Desenvolvido por Cleverton Teixeira 2022</p>;
+}
