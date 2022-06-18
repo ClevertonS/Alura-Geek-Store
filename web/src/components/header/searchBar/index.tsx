0f3bc309495a441ee467f3b207f103b3f078a376
Cleverton Teixeira <clevertons.b.t@gmail.com>
2022-06-17 23:11:43 -0300

    adding search bar Mobile

--- web/src/components/header/searchBar/index.tsx
+++ web/src/components/header/searchBar/index.tsx
@@ -4,8 +4,8 @@ import React, { useState } from 'react';
 export function SearchBar() {
   const [searchValue, setSearchValue] = useState('');
   return (
-    <div className="invisible h-0 w-0 rounded-full bg-black-5 sm:visible sm:h-10 sm:w-1/2 sm:min-w-[250px] sm:max-w-[393px] xl:w-[393px] ">
-      <form action="" className="flex items-center justify-between px-4 py-2">
+    <div className="mx-4 flex rounded-full bg-black-5 sm:mx-0 sm:h-10 sm:w-1/2 sm:min-w-[250px] sm:max-w-[393px] xl:w-[393px] ">
+      <form action="" className="flex w-full items-center justify-between px-4 py-2">
         <input
           onChange={(event) => setSearchValue(event.target.value)}
           value={searchValue}
